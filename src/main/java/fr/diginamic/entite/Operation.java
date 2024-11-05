661562c1abed2f6d17238e56a0bd24ccd9c85715
johan guillen <sioupes34@gmail.com>
2024-11-05 17:47:54 +0100

    operation fait mise en place de l'heritage avec virement

--- src/main/java/fr/diginamic/entite/Operation.java
+++ src/main/java/fr/diginamic/entite/Operation.java
@@ -1,4 +1,21 @@
 package fr.diginamic.entite;
 
-public class Operation {
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Table
+public abstract class Operation {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID")
+    private Integer id;
+    @Column(name = "DATE")
+    private LocalDateTime date;
+    @Column(name = "MONTANT")
+    private double montant;
+    @Column(name = "MOTIF")
+    private String motif;
 }
