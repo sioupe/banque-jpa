126ab217acd42fe68b92f1918f11c5f1ebcdf92c
johan guillen <sioupes34@gmail.com>
2024-11-05 17:31:05 +0100

    client fait

--- src/main/java/fr/diginamic/entite/Client.java
+++ src/main/java/fr/diginamic/entite/Client.java
@@ -1,4 +1,22 @@
 package fr.diginamic.entite;
 
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+@Entity
+@Table(name = "client")
 public class Client {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID")
+    private Integer id;
+
+    @Column (name = "NOM")
+    private String nom;
+    @Column (name = "PRENOM")
+    private String prenom;
+    @Column (name = "DATE_NAISSANCE")
+    private LocalDate dateNaissance;
+
+
 }
